4e57877d302de6036102e25cf0d79fee935bfa36
hurutomomk <hurutomomk@gmail.com>
2022-01-16 23:00:30 +0900

    (0010)MapGeneratorスクリプトを登録。メインシーンにオブジェクトを生成しDontDestroyOnLoad対象に指定。

---
+++ Assets/ChoiJihun/Scripts/Manager/MapGenerator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    #region [var]
+
+    #region [01. Instance]
+    
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    public static MapGenerator Instance { get; private set; }
+
+    #endregion
+    
+    
+
+    #endregion
+
+    #region [func]
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private void Awake()
+    {
+        
+    }
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private void Start()
+    {
+        // インスタンス
+        Instance = this;
+        // 破棄不可
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    #endregion
+}
