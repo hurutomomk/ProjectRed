0e7737ea330411888c5ee4523d44fc0b4402d0a0
hurutomomk <hurutomomk@gmail.com>
2022-04-26 17:31:56 +0900

    (0013)簡易的なPlayerMovementを実装。

--- Assets/ChoiJihun/Scripts/Game/Controller/PlayerMovementController.cs
+++ Assets/ChoiJihun/Scripts/Game/Controller/PlayerMovementController.cs
@@ -1,39 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // コルーチン
-    private Coroutine coroutine;
-    
+    #region [var]
+
+    #region [01. ボタン押下判定関連]
     /// <summary>
-    /// 
+    /// 移動スピード
+    /// </summary>
+    [SerializeField]
+    private float moveSpeed = 5f;
+    /// <summary>
+    /// Pointer座標
+    /// </summary>
+    [SerializeField]
+    private Transform pointer;
+    #endregion
+    
+    #endregion
+
+    #region [func]
+
+    #region [00. コンストラクタ] 
+    /// <summary>
+    /// コンストラクタ
     /// </summary>
     public void ActivePlayerMovement()
     {
+        // Pointer座標初期化
+        this.pointer.position = this.transform.position;
+        
+        // 移動ボタン押下判定コルーチンの開始準備
         this.CatchPlayerMovementInputAsync();
     }
+    #endregion
 
+    #region [01. 移動ボタン入力判定]
     /// <summary>
-    /// 移動ボタン押下判定コルーチンの開始準備
+    /// 移動ボタン入力コルーチンの開始準備
     /// </summary>
     private void CatchPlayerMovementInputAsync()
     {
-        // if (this.coroutine != null)
-        // {
-        //     StopCoroutine(this.coroutine);
-        // }
-        //
-        // // コルーチンスタート
-        // this.coroutine = StartCoroutine(this.CatchPlayerMovementInput());
-
         // コルーチンスタート
         GlobalCoroutine.Play(this.CatchPlayerMovementInput(), "CatchPlayerMovementInput", null);
     }
 
     /// <summary>
-    /// 移動ボタン押下判定コルーチン
+    /// 移動入力判定コルーチン
     /// </summary>
     /// <returns></returns>
     IEnumerator CatchPlayerMovementInput()
@@ -42,8 +56,39 @@ public class PlayerMovementController : MonoBehaviour
         
         while (true)
         {
+            // Player移動
+            this.MoveToPoint(this.pointer.position);
+
+            // 入力判定
+            if (Vector3.Distance(transform.position, pointer.position) <= .05f)
+            {
+                // 横移動
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+                {
+                    this.pointer.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+                // 縦移動
+                if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                {
+                    this.pointer.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
             
             yield return null;
         }
     }
+
+    /// <summary>
+    /// Player移動
+    /// </summary>
+    /// <param name="point"></param>
+    private void MoveToPoint(Vector3 point)
+    {
+        // 移動
+        this.transform.position =
+            Vector3.MoveTowards(this.transform.position, point, moveSpeed * Time.deltaTime);
+    }
+    #endregion
+   
+    #endregion
 }
